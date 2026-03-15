b6476649d7777b3824a2dac30c247527249c7fac
oilPersonage <oilfreelance@ya.ru>
2026-03-15 18:04:39 +0300

    test deploy 2

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  assetPrefix: isProd ? "/den/" : undefined,
+  assetPrefix: isProd ? "/oilpersonage/den/" : undefined,
   skipTrailingSlashRedirect: true,
   images: { unoptimized: true },
   typescript: {
