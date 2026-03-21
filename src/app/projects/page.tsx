849c05830a8bc78b349dd1a918f8c1ee79fafe6a
oilPersonage <oilfreelance@ya.ru>
2026-03-21 20:44:15 +0300

    test projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,9 +4,70 @@ export default function Projects() {
   return (
     <>
       <Navs page="/projects" />
-      <section className="main-wrapper pt-header min-h-full">
-        <h2 className="py-lg">PROJECT</h2>
-      </section>
+      <ul className="flex">
+        <div className="relative">
+          <h2 className="sticky pl-lg top-26">Проекты</h2>
+          <h4 className="sticky pl-lg top-42 h-screen">Лукоил</h4>
+          <h4 className="sticky pl-lg top-52 h-screen">Трансерфинг</h4>
+          <h4 className="sticky pl-lg top-62 h-screen">МФЦ</h4>
+          <h2 className="sticky pl-lg pt-lg top-72 h-screen bg-bg2">
+            Фотографии
+          </h2>
+        </div>
+
+        {/* Правая колонка с контентом */}
+        <div className="flex-1 pt-9">
+          <div className="h-screen flex items-center px-lg">
+            <div>Контент для Лукоила</div>
+          </div>
+          <div className="h-screen flex items-center px-lg">
+            <div>Контент для Трансерфинга</div>
+          </div>
+          <div className="h-screen flex items-center px-lg">
+            <div>Контент для МФЦ</div>
+          </div>
+          <div className="min-h-screen flex px-lg bg-bg2 py-lg">
+            <div className="min-h-screen w-full">
+              <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] *:bg-bg gap-1 *:min-h-40">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ul>
     </>
   );
 }
